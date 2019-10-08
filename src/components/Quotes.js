354a035e6827006a93f5cb22d27e876ffbc8b02f
Ollie Pegram <ollie.pegram@biogen.com>
2019-10-08 14:57:29 +1100

    Quote cards now hide on incorrect guesses

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,71 +1,75 @@
-import React, { Component } from 'react';
-import 'hover.css';
-import './Quotes.css';
+import React, { Component } from "react";
+import "hover.css";
+import "./Quotes.css";
 
 class Quotes extends Component {
-    listRef = React.createRef();
+  state = { activeItem: -1 };
+  listRef = React.createRef();
 
-    render() {
-        if (this.props.gameWon) {
-            return (
-                <div className='QuoteContainer'>
-                    <ul className='AllQuotes'>
-                        {this.props.quotes.map(quote => (
-                            <li
-                                key={quote.id}
-                                className='SingleQuote'
-                                style={{
-                                    backgroundColor: this.props.author.color
-                                }}
-                            >
-                                <h3 className='quote'>
-                                    {this.props.author.quote}
-                                </h3>
-                                <div className='quote-about'>
-                                    <span className='quote-season'>
-                                        Season: {this.props.author.season}
-                                    </span>
-                                    <span className='quote-episode'>
-                                        Episode: {this.props.author.episode}
-                                    </span>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+  handleClickItem(index) {
+    this.setState({ activeItem: index });
+  }
+
+  render() {
+    if (this.props.gameWon) {
+      return (
+        <div className='QuoteContainer'>
+          <ul className='AllQuotes'>
+            {this.props.quotes.map(quote => (
+              <li
+                key={quote.id}
+                className='SingleQuote hvr-float'
+                style={{
+                  backgroundColor: this.props.author.color
+                }}
+              >
+                <h3 className='quote'>{this.props.author.quote}</h3>
+                <div className='quote-about'>
+                  <span className='quote-season'>
+                    Season: {this.props.author.season}
+                  </span>
+                  <span className='quote-episode'>
+                    Episode: {this.props.author.episode}
+                  </span>
                 </div>
-            );
-        } else if (this.props.gameWon === false) {
-            return (
-                <div className='QuoteContainer'>
-                    <ul className='AllQuotes'>
-                        {this.props.quotes.map(quote => (
-                            <li
-                                key={quote.id}
-                                className='SingleQuote hvr-float'
-                                style={{
-                                    backgroundColor: quote.color
-                                }}
-                                onClick={() =>
-                                    this.props.handleChoice(quote.id)
-                                }
-                                ref={this.listRef}
-                            >
-                                <h3 className='quote'>{quote.quote}</h3>
-                                <div className='quote-about'>
-                                    <span className='quote-season'>
-                                        Season: {quote.season}
-                                    </span>
-                                    <span className='quote-episode'>
-                                        Episode: {quote.episode}
-                                    </span>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    } else if (this.props.gameWon === false) {
+      return (
+        <div className='QuoteContainer'>
+          <ul className='AllQuotes'>
+            {this.props.quotes.map((quote, index) => (
+              <li
+                key={quote.id}
+                className={`${
+                  this.state.activeItem === index ? "SingleQuote-hidden" : ""
+                } SingleQuote hvr-float`}
+                style={{
+                  backgroundColor: quote.color
+                }}
+                onClick={() => {
+                  this.props.handleChoice(quote.id);
+                  this.handleClickItem(index);
+                }}
+                ref={this.listRef}
+              >
+                <h3 className='quote'>{quote.quote}</h3>
+                <div className='quote-about'>
+                  <span className='quote-season'>Season: {quote.season}</span>
+                  <span className='quote-episode'>
+                    Episode: {quote.episode}
+                  </span>
                 </div>
-            );
-        }
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
     }
+  }
 }
 
 export default Quotes;
